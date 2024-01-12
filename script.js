49adc23eb61ffceb21fd3070ad0c0f134ebff9df
ayushlostvolts <153606859+ayushlostvolts@users.noreply.github.com>
2024-01-12 17:50:58 +0530

    added scripts file

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// Your jQuery code goes here
+$(document).ready(function() {
+    // Your jQuery functions
+
+    // For example, you can add a click event to a button
+    $('#myButton').click(function() {
+        alert('Button clicked!');
+    });
+});
